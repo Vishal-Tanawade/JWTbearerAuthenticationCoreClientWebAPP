62235b258094ee8e166cdb71b43e6a5e6fbbfbde
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-06 10:54:24 +0530

    Session  added in Startup

--- JWTbearerAuthenticationCoreClientWebAPP/Startup.cs
+++ JWTbearerAuthenticationCoreClientWebAPP/Startup.cs
@@ -23,6 +23,7 @@ namespace JWTbearerAuthenticationCoreClientWebAPP
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,12 +42,13 @@ namespace JWTbearerAuthenticationCoreClientWebAPP
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=JWTClient}/{action=Index}/{id?}");
             });
         }
     }
