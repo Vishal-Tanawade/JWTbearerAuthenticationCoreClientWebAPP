ff8495815f5edc011b5b3eee91286bd20569c841
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-06 10:21:32 +0530

    MVC Controller with read/Write action added

---
+++ JWTbearerAuthenticationCoreClientWebAPP/Controllers/JWTClientController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTbearerAuthenticationCoreClientWebAPP.Controllers
+{
+    public class JWTClientController : Controller
+    {
+        // GET: JWTClientController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: JWTClientController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: JWTClientController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: JWTClientController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: JWTClientController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: JWTClientController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: JWTClientController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: JWTClientController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
