c9c9774a985e9dbf1b057192b452d9863ae919a7
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-06 10:37:35 +0530

    Controller Modified Index.cshtml -- minor changes

--- JWTbearerAuthenticationCoreClientWebAPP/Controllers/JWTClientController.cs
+++ JWTbearerAuthenticationCoreClientWebAPP/Controllers/JWTClientController.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JWTbearerAuthenticationCoreClientWebAPP.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace JWTbearerAuthenticationCoreClientWebAPP.Controllers
@@ -14,6 +20,88 @@ namespace JWTbearerAuthenticationCoreClientWebAPP.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserModel userModel)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(userModel), Encoding.UTF8, "application/json");
+
+
+                using (var response = await httpClient.PostAsync("http://localhost:1635/api/JWTUserrAuthService/Authenticate/", content))
+                {
+                    //string apiResponse = await response.Content.ReadAsStringAsync();
+
+                    string stringJWT = response.Content.ReadAsStringAsync().Result;
+
+
+                    JWT jwt = JsonConvert.DeserializeObject<JWT>(stringJWT);
+                    if (jwt.Token != null)
+                    {
+                        HttpContext.Session.SetString("token", jwt.Token);
+                        ViewBag.Message = "User logged in successfully!";
+                        return View("UserHome");
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Invalid email or password!";
+                        return RedirectToAction("Index");
+                    }
+
+
+
+                }
+
+
+            }
+        }
+
+        [HttpGet]
+        public ActionResult UserHome()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        // GET: JWTClientController/ShowData
+        public IActionResult ShowData()
+        {
+            string baseUrl = "http://localhost:1635/";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(baseUrl);
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+
+            client.DefaultRequestHeaders.Authorization =
+        new AuthenticationHeaderValue("Bearer",
+        HttpContext.Session.GetString("token"));
+
+            HttpResponseMessage response = client.GetAsync("api/JWTUserrAuthService").Result;
+            string stringData = response.Content.ReadAsStringAsync().Result;
+            List<Product> data = JsonConvert.DeserializeObject<List<Product>>(stringData);
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                ViewBag.Message = "Unauthorized!";
+            }
+            else
+            {
+
+                return View(data);
+            }
+            ViewBag.Message = "User logged out successfully!";
+            return View("Index");
+        }
+        // GET: JWTClientController/Logout
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("token");
+            ViewBag.Message = "User logged out successfully!";
+            return View("Index");
+        }
+
 
         // GET: JWTClientController/Details/5
         public ActionResult Details(int id)
