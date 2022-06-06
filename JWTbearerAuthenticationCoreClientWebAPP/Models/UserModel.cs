308e41f744298a0921d4d5c59811139c59ec595c
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-06 10:09:13 +0530

    POCO Classes Added

---
+++ JWTbearerAuthenticationCoreClientWebAPP/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTbearerAuthenticationCoreClientWebAPP.Models
+{
+    public class UserModel
+    {
+        [DisplayName("Email")]
+        [EmailAddress]
+        [Required(ErrorMessage = "Enail id can`t be blank")]
+        public string Email { get; set; }
+
+        [DisplayName("Password")]
+        [DataType(DataType.Password)]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")]
+        [Required(ErrorMessage = "Password can`t be blank")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ JWTbearerAuthenticationCoreClientWebAPP/Models/UserType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTbearerAuthenticationCoreClientWebAPP.Models
+{
+    //This class used at the time of login for dropdown list user type
+    public class UserType
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserTypeId { get; set; }
+
+        [DisplayName("User Type")]
+        [Required(ErrorMessage = "Login type should not be blank eg. Manager / User")]
+        [StringLength(30, MinimumLength = 5)]
+        public string UserTypeName { get; set; }
+
+    }
+}
